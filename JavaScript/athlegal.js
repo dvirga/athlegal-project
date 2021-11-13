1fb906b95ee1890000c01608290e162284a7d061
dvirga <93686499+dvirga@users.noreply.github.com>
2021-11-13 09:04:08 -0500

    updated project
    updated home page, css, js and images file

--- JavaScript/athlegal.js
+++ JavaScript/athlegal.js
@@ -1,4 +1,36 @@
-/*
-JavaScript file goes here
-hi ladies
-*/
+//back to top button
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+//hamburger menu
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
+
+hamburger.addEventListener('click', ()  => {
+  navUL.classList.toggle('show');
+});
